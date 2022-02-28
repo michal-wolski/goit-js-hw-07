d6cb6b8e6e6fda74359ca5782de5abb62b4fff2a
michal-wolski <michal.wolski130@gmail.com>
2022-02-28 12:04:58 +0100

    add basiclight and scritp

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryBox = document.querySelector("div.gallery");
+
+console.log(galleryItems);
+
+for (const el of galleryItems) {
+  const galleryItem = document.createElement("div");
+  galleryItem.classList.add("gallery__item");
+
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.href = el.original;
+
+  const galleryImage = document.createElement("img");
+  galleryImage.classList.add("gallery__image");
+  galleryImage.src = el.preview;
+  galleryImage.dataset.source = el.original;
+  galleryImage.alt = el.description;
+
+  galleryBox.append(galleryItem);
+  galleryItem.append(galleryLink);
+  galleryLink.append(galleryImage);
+};
+galleryBox.addEventListener("click", ev => {
+    event.preventDefault();
+    const instance = basicLightbox.create(`
+<img src=${event.target.dataset.source} width="1280">
+`)
+
+instance.show()
+})
+
