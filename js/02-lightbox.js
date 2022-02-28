803139e1fe20b9e245bdce2de7c34dd673088e49
michal-wolski <michal.wolski130@gmail.com>
2022-02-28 13:23:19 +0100

    tast 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,3 +3,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryBox = document.querySelector("ul.gallery");
+
+for (const el of galleryItems) {
+
+
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.href = el.original;
+
+  const galleryImage = document.createElement("img");
+  galleryImage.classList.add("gallery__image");
+  galleryImage.src = el.preview;
+  galleryImage.alt = el.description;
+  galleryBox.append(galleryLink);
+  galleryLink.append(galleryImage);
+};
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+console.log(lightbox.captionsData)
